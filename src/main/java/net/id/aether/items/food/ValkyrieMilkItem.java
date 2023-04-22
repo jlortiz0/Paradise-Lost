6eef90d5aa01fd57d493daea4c3db180b439867c
jlortiz0 <jlortiz0@users.noreply.github.com>
2023-04-22 00:45:12 -0700

    Add a terrible voice line

--- src/main/java/net/id/aether/items/food/ValkyrieMilkItem.java
+++ src/main/java/net/id/aether/items/food/ValkyrieMilkItem.java
@@ -1,13 +1,19 @@
 package net.id.aether.items.food;
 
 import net.id.aether.items.AetherItems;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class ValkyrieMilkItem extends Item {
     public ValkyrieMilkItem(Settings settings) {
@@ -23,5 +29,14 @@ public class ValkyrieMilkItem extends Item {
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
         super.finishUsing(stack, world, user);
         return stack.isEmpty() ? new ItemStack(AetherItems.QUICKSOIL_VIAL) : stack;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(new TranslatableText("item.the_aether.valkyrie_milk.description1"));
+        for (int i = 0; i < 2; i++) {
+            tooltip.add(new TranslatableText("item.the_aether.valkyrie_milk.description2"));
+        }
+        tooltip.add(new TranslatableText("item.the_aether.valkyrie_milk.description3"));
     }
 }
